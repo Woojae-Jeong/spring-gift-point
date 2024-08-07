4ad914931c349bc587d9676a738dbc2ec1ef9f17
Woojae Jeong <woo6388@naver.com>
2024-08-07 18:19:05 +0900

    refactor : 사용하지 않는 클래스 삭제

--- src/main/java/gift/Exception/InvalidEditTypeException.java
+++
@@ -1,7 +0,0 @@
-package gift.Exception;
-
-public class InvalidEditTypeException extends BusinessException {
-    public InvalidEditTypeException (String message){
-        super(message);
-    }
-}
