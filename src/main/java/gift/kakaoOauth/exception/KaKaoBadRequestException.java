3a0ec0595bb0f7ef1d03cd0efa9330e316ee8aa6
Woojae Jeong <woo6388@naver.com>
2024-08-07 20:39:09 +0900

    refactor : 카카오 API BadRequest 세분화 하기위해 삭제

--- src/main/java/gift/kakaoOauth/exception/KaKaoBadRequestException.java
+++
@@ -1,9 +0,0 @@
-package gift.kakaoOauth.exception;
-
-import gift.exception.BusinessException;
-
-public class KaKaoBadRequestException extends BusinessException {
-    public KaKaoBadRequestException(String message){
-        super(message);
-    }
-}
