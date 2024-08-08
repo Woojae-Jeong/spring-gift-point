91910b6c01739fb5c6dd74440f49a5db1b8f51fc
Woojae Jeong <woo6388@naver.com>
2024-08-08 18:27:11 +0900

    feat : 경로 변수와 쿼리 파라미터가 빠진 경우 발생한 예외에 대해 Error Code 구조화 적용

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,17 +27,40 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(response, errorType.getHttpStatus());
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(
+            MissingPathVariableException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ErrorType errorType = commonErrorType.MISSING_PATH_VARIABLE_EXCEPTION;
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getMessage());
+        return new ResponseEntity<>(response, errorType.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ErrorType errorType = commonErrorType.MISSING_REQUEST_PARAM_EXCEPTION;
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getMessage());
+        return new ResponseEntity<>(response, errorType.getHttpStatus());
+    }
+
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorType errorType = e.getErrorType();
-        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getErrorCode());
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getMessage());
         return new ResponseEntity<>(response, errorType.getHttpStatus());
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
         ErrorType errorType = commonErrorType.NO_SUCH_ARGUMENT_EXCEPTION;
-        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getErrorCode());
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getMessage());
         return new ResponseEntity<>(response, errorType.getHttpStatus());
     }
 
@@ -43,7 +68,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorType errorType = commonErrorType.ILLEGAL_ARGUMENT_EXCEPTION;
-        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getErrorCode());
+        ErrorResponse response = new ErrorResponse(errorType.getErrorCode(), errorType.getHttpStatus(), errorType.getMessage());
         return new ResponseEntity<>(response, errorType.getHttpStatus());
     }
 }
