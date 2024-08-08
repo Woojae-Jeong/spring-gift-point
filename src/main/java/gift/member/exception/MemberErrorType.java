7b8853abd65159c4766530458bd2512a9585af35
Woojae Jeong <woo6388@naver.com>
2024-08-08 13:44:41 +0900

    refactor : Member 에러코드 이름 변경

--- src/main/java/gift/member/exception/MemberErrorType.java
+++ src/main/java/gift/member/exception/MemberErrorType.java
@@ -4,9 +4,9 @@ import gift.exception.ErrorType;
 import org.springframework.http.HttpStatus;
 
 public enum MemberErrorType implements ErrorType {
-    MEMBER_NOT_FOUND_EXCEPTION("M404_001", HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다"),
-    INVALID_CREDENTIAL_EXCEPTION("M401_001", HttpStatus.UNAUTHORIZED, "이메일이나 비밀번호가 틀렸습니다"),
-    EMAIL_ALREADY_EXISTS_EXCEPTION("M409_001", HttpStatus.CONFLICT, "이미 존재하는 이메일입니다");
+    MEMBER_NOT_FOUND_EXCEPTION("Member404_001", HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다"),
+    INVALID_CREDENTIAL_EXCEPTION("Member401_001", HttpStatus.UNAUTHORIZED, "이메일이나 비밀번호가 틀렸습니다"),
+    EMAIL_ALREADY_EXISTS_EXCEPTION("Member409_001", HttpStatus.CONFLICT, "이미 존재하는 이메일입니다");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
