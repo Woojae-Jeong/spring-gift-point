f868e5131ce7a9a14b5385c53ca1350de099a3da
Woojae Jeong <woo6388@naver.com>
2024-08-08 13:44:20 +0900

    refactor : 카카오 API 에러코드 이름 변경

--- src/main/java/gift/kakaoOauth/exception/KakaoOauthErrorType.java
+++ src/main/java/gift/kakaoOauth/exception/KakaoOauthErrorType.java
@@ -4,11 +4,11 @@ import gift.exception.ErrorType;
 import org.springframework.http.HttpStatus;
 
 public enum KakaoOauthErrorType implements ErrorType {
-    KAKAO_USER_INFO_BAD_REQUEST_EXCEPTION("K400_001",HttpStatus.BAD_REQUEST, "카카오 정보 가져오기 API : BadRquest 발생"),
-    KAKAO_TOKEN_BAD_REQUEST_EXCEPTION("K400_002", HttpStatus.BAD_REQUEST, "카카오 토큰 가져오기 API : BadRquest 발생"),
-    KAKAO_SEND_MESSAGE_TO_ME_BAD_REQUEST_EXCEPTION("K400_003", HttpStatus.BAD_REQUEST, "카카오 정보 가져오기 API : BadRquest 발생"),
-    JASON_RUN_TIME_EXCEPTION("K500_001", HttpStatus.INTERNAL_SERVER_ERROR, "카카오 나에게 메세지 보내기 API : Json 객체 변환 중 에러 발생"),
-    KAKAO_SERVER_ERROR_EXCEPTION("K502_001", HttpStatus.BAD_GATEWAY, "카카오 API : 카카오 서버 에러 발생");
+    KAKAO_USER_INFO_BAD_REQUEST_EXCEPTION("Kakao400_001",HttpStatus.BAD_REQUEST, "카카오 정보 가져오기 API : BadRquest 발생"),
+    KAKAO_TOKEN_BAD_REQUEST_EXCEPTION("Kakao400_002", HttpStatus.BAD_REQUEST, "카카오 토큰 가져오기 API : BadRquest 발생"),
+    KAKAO_SEND_MESSAGE_TO_ME_BAD_REQUEST_EXCEPTION("Kakao400_003", HttpStatus.BAD_REQUEST, "카카오 정보 가져오기 API : BadRquest 발생"),
+    JASON_RUN_TIME_EXCEPTION("Kakao500_001", HttpStatus.INTERNAL_SERVER_ERROR, "카카오 나에게 메세지 보내기 API : Json 객체 변환 중 에러 발생"),
+    KAKAO_SERVER_ERROR_EXCEPTION("Kakao502_001", HttpStatus.BAD_GATEWAY, "카카오 API : 카카오 서버 에러 발생");
 
 
     private final String errorCode;
